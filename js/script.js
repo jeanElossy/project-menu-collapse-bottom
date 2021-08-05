c19ab11c5fbb98f3a60b14a34686b8fb795e315c
jean elossy <jean.elossy@yahoo.com>
2021-08-05 05:13:46 +0200

    Mise a jour et fin projet

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.querySelector(".fa-chevron-down").addEventListener("click", collapse);
 function collapse(){
-  this.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].style.transform = "translateY(30px)";
-  this.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].style.visibility = "visible";
+  this.parentElement.parentElement.parentElement.parentElement.nextElementSibling.children[0].classList.toggle("collapse");
+  this.classList.toggle("change__sens");
   
 }
